7203044bcf7e74fd9e21211c4dfd767c0c7625c5
evobg <egorbogaev@gmail.com>
2021-02-11 16:35:50 +0300

    WebServer1 ADD

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-132
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+	http.ListenAndServe(":80", nil)
+	fmt.Println("123")
+}
